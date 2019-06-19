b67cea302d5b6eb707ce2c0e5a7212ecb132e8f1
shengzhijie <1158073764@qq.com>
2019-06-19 21:41:43 +0800

    添加项目文件。

---
+++ StudyLayUI/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using StudyLayUI.Models;
+
+namespace StudyLayUI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> GetInitData()
+        {
+            var jsonPath=Path.Combine(_environment.WebRootPath, @"lib\layui\getList.json");
+            var str = await System.IO.File.ReadAllTextAsync(jsonPath);
+            return Content(str);
+        }
+
+        public async Task<IActionResult> GetMembers()
+        {
+            var jsonPath = Path.Combine(_environment.WebRootPath, @"lib\layui\getMembers.json");
+            var str = await System.IO.File.ReadAllTextAsync(jsonPath);
+            return Content(str);
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ StudyLayUI/Hubs/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace StudyLayUI.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+    }
+}
